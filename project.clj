7f4065ddd8c4bfc2be838ea2eec09f3310273544
Tetsuya Matsuyama <matsuyama.tetsuya@gmail.com>
2014-01-09 14:17:39 +0900

    update

--- project.clj
+++ project.clj
@@ -1,9 +1,5 @@
 (defproject heroku-sample "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :main heroku-sample.core          
   :uberjar-name "heroku-sample-standalone.jar"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.5.1"]
-                           [ring/ring-jetty-adapter "1.1.6"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-jetty-adapter "1.1.6"]]
+  :min-lein-version "2.0.0")
